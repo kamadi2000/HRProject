651633b180ab123dd2b665d7f78868ef3ec4dea9
Thamindumk <thamindu.20@cse.mrt.ac.lk>
2022-12-28 09:58:04 +0530

    create token method

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const Method = require("../controllers/method");
+const jwt = require('jsonwebtoken');
+const {config} = require('../database/config')
+const {auth} = require('../middleware/auth')
+require('dotenv').config();
 
 const {ResponseHandler} = require("../controllers/ResponseController");
 const {UserController} = require("../controllers/UserController");
@@ -8,15 +11,47 @@ const router = express.Router();
 
 const controller = new UserController();
 
-router.get('/', (req,res)=>{
-    console.log("Happy");
+router.get('/',auth, (req,res)=>{
+    const user = req.user;
+    res.json(user);
 })
 
 router.post('/login', async (req,res)=>{
-    
-    const status = await controller.login(req);
 
-    res.status(ResponseHandler(status)).send(status);
+    const status = await controller.login(req);
+    if(status){
+        const user = {type:status[0].job_tittle, username: status[0].emp_ID}
+        const accesstoken = jwt.sign(user,config.ACCESS_TOKEN_KEY,{expiresIn: '10s'});
+        const refreshtoken = jwt.sign(user,config.REFRESH_TOKEN_KEY,{expiresIn: '15m'});
+        await controller.storeToken(refreshtoken,status[0].emp_ID);
+        res.status(ResponseHandler(status)).send({accesstoken,refreshtoken});
+    }else{
+        res.sendStatus(401)
+    }
 });
+
+router.post('/token', async (req,res)=>{
+    const refreshtoken = req.body.refreshtoken;
+    if(refreshtoken==null) res.sendStatus(401);
+    const status = await controller.getToken(refreshtoken);
+    try{
+        if(status=="token not available"){
+            res.sendStatus(403);
+        }else{
+            jwt.verify(refreshtoken,config.REFRESH_TOKEN_KEY, (err,user)=>{
+                if(err) res.sendStatus(403);
+                const accesstoken = jwt.sign({type:user.type, username: user.username},config.ACCESS_TOKEN_KEY,{expiresIn: '10s'});
+                res.status(ResponseHandler(status)).send({accesstoken});
+            })
+        }
+    }catch(e){
+        console.log(e)
+    }
+})
+
+router.delete('/logout',(req,res)=>{
+    controller.deleteToken(req.body.refreshtoken)
+    res.sendStatus(204);
+})
 
 module.exports = router;
