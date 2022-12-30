8f574bcf1b11fe4c9df2275f1fcb4cbe9a3afd10
thamindumk <119829428+thamindumk@users.noreply.github.com>
2022-12-31 00:54:59 +0530

    Revert "User thamindu"

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,8 +1,6 @@
 exports.config= {
     HOST: 'localhost',
     USER: 'root',
-    PASSWORD: '#200328Ptmk',
-    DATABASE: 'project',
-    ACCESS_TOKEN_KEY: "HRProject16",
-    REFRESH_TOKEN_KEY: "Team16",
+    PASSWORD: 'Rock@2000',
+    DATABASE: 'sakila'
 }
