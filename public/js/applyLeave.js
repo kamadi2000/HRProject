f44c228232ebd594c5531de03e51b18b90878ab6
kamadi2000 <kmaliyanage@gmail.com>
2023-01-03 23:32:41 +0530

    leave file changes

--- public/js/applyLeave.js
+++ public/js/applyLeave.js
@@ -1,6 +1,5 @@
 const emp_ID = document.getElementById('emp_ID');
 const reason = document.getElementById('reason');
-const Ndays = document.getElementById('Ndays');
 const date = document.getElementById('date');
 const leave_type = document.getElementById('leave-type');
 const submitbtn = document.getElementById('submit-btn');
@@ -8,7 +7,7 @@ const submitbtn = document.getElementById('submit-btn');
 
 
 submitbtn.addEventListener('click',()=>{
-    // console.log(username.value, password.value)
+    console.log(emp_ID.value, reason.value,date.value,leave_type.value)
     const accesstoken = localStorage.getItem('Accesstoken');
     const refreshtoken = localStorage.getItem('Refreshtoken');
     fetch('/user/applyleave',{
@@ -34,7 +33,7 @@ submitbtn.addEventListener('click',()=>{
             })
         })
         .then(res => res.json())
-        .then(data => {
+        .then(data => { 
             localStorage.setItem('Accesstoken',data.accesstoken);
             const accesstoken = localStorage.getItem('Accesstoken');
             fetch('/user/applyleave',{
@@ -42,17 +41,19 @@ submitbtn.addEventListener('click',()=>{
                 headers:{'Content-Type':'application/json',
                         'authorization':`bearer ${accesstoken}`},
                 body:JSON.stringify({
-                    username:emp_ID.value,
+                    // username:emp_ID.value,
                     reason:reason.value,
                     date:date.value,
                     leave_type:leave_type.value,
                 
             })
+         })
             .then(res=> res.json())
             .then(data => {
-                console.log(data)
+                console.log(data);
             })
-        })
+        
     })
 })
 })
+
