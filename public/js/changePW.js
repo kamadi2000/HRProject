5581331d488c13e84c00adff673c227e134807d9
kamadi2000 <kmaliyanage@gmail.com>
2023-01-08 14:54:04 +0530

    changePassword pages

---
+++ public/js/changePW.js
@@ -0,0 +1,58 @@
+const oldPassword = document.getElementById('oldPassword');
+const newPassword = document.getElementById('newPassword');
+const confirmPassword = document.getElementById('confirmPassword');
+const submitbtn = document.getElementById('submit-btn');
+
+submitbtn.addEventListener('click',()=>{
+    const accesstoken = localStorage.getItem('Accesstoken');
+    const refreshtoken = localStorage.getItem('Refreshtoken');
+    fetch('/user/changepassword',{
+        method:'post',
+        headers:new Headers({'Content-Type':'application/json','authorization':`bearer ${accesstoken}`}),
+        body:JSON.stringify({
+            oldPassword : oldPassword.value,
+            newPassword:newPassword.value,
+            confirmPassword : confirmPassword.value,
+
+        })
+    })
+    .then(res => res.json())
+    .then(data=> {
+        console.log(data);
+        alert(data.message);
+        location.href = '/changePassword';
+
+    })
+    .catch ((err) => {
+        fetch('/auths/token',{
+            method:'post',
+            headers:new Headers({'Content-Type':'application/json'}),
+            body:JSON.stringify({
+                refreshtoken:refreshtoken
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            localStorage.setItem('Accesstoken',data.accesstoken);
+            const accesstoken = localStorage.getItem('Accesstoken');
+            fetch('/user/changepassword ',{
+                method:'post',
+                headers:new Headers({'Content-Type':'application/json',
+                        'authorization':`bearer ${accesstoken}`}),
+                body:JSON.stringify({
+                    oldPassword : oldPassword.value,
+                    newPassword:newPassword.value,
+                    confirmPassword : confirmPassword.value,
+                        })
+                
+                
+            })
+            .then(res=> res.json())
+            .then(data => {
+                console.log(data)
+                alert(data.message);
+                location.href = '/changePassword';
+            })
+        })
+    })
+})
