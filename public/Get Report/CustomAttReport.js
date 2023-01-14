c59534d7bf02da8bcb04c18a9701442e37873bd8
Yasira Punsith <yasirapunsith@gmail.com>
2023-01-14 09:17:46 +0530

    custom field report

--- public/Get Report/CustomAttReport.js
+++ public/Get Report/CustomAttReport.js
@@ -1,6 +1,3 @@
-const field = document.getElementById('field');
-const value = document.getElementById('value');
-const submitbtn = document.getElementById('submit-btn');
 
 submitbtn.addEventListener('click',()=>{
     const accesstoken = localStorage.getItem('Accesstoken');
@@ -39,6 +36,101 @@ submitbtn.addEventListener('click',()=>{
                     field:field.value,
                     value:value.value
                         })
+                
+                
+            })
+            .then(res=> res.json())
+            .then(data => {
+                console.log(data);
+            })
+        })
+    })
+})
+
+
+
+const field = document.getElementById('field');
+const value = document.getElementById('value');
+const submitbtn = document.getElementById('submit-btn');
+
+
+submitbtn.addEventListener('click',()=>{
+    const accesstoken = localStorage.getItem('Accesstoken');
+    const refreshtoken = localStorage.getItem('Refreshtoken');
+    fetch('/reportemployee/custom',{
+        method:'post',
+        headers:new Headers({'Content-Type':'application/json','authorization':`bearer ${accesstoken}`}),
+        body:JSON.stringify({
+            field:field.value,
+            value:value.value
+        })
+    })
+    .then(res => res.json())
+    .then(data=> {
+        console.log(data);
+        
+        let element = document.getElementById("block");
+        while (element) {
+            element.parentNode.removeChild(element);
+            element = document.getElementById("block");
+        }
+
+        data.forEach(element => {
+            const markup = `
+            <div id="block">
+            <div class="tableRow">
+            <div class="cell" style="width:20%;">${element.ID}</div>
+            <div class="cell" style="width: 40%;">
+                <ul>
+                    <li>First Name: ${element.first_name}</li>
+                    <li>Middle Name: ${element.middle_name}</li>
+                    <li>Last Name: ${element.last_name}</li>
+                    <li>Date Of Birth: ${element.date_of_birth}</li>
+                    <li>Gender: ${element.gender}</li>
+                    <li>Marital Status: ${element.marital_status}</li>
+                    <li>Road: ${element.road}</li>
+                    <li>City: ${element.city}</li>
+                    <li>Country: ${element.country}</li>                    
+                </ul>
+            </div>
+            <div class="cell" style="width: 40%;">
+                <ul>
+                    <li>Branch ID: ${element.branch_ID}</li>
+                    <li>Department: ${element.department}</li>
+                    <li>Job Title: ${element.job_title}</li>
+                    <li>Paygrade: ${element.pay_grade}</li>
+                    <li>Employeement Status: ${element.employeement_status}</li>
+                    <li>Working Time: ${element.working_time}</li>
+                </ul>
+            </div>
+        </div>
+        </div>`
+        document.querySelector('.tableContent').insertAdjacentHTML('beforeend', markup);
+        });
+
+            
+
+})
+    .catch ((err) => {
+        fetch('/auths/token',{
+            method:'post',
+            headers:new Headers({'Content-Type':'application/json'}),
+            body:JSON.stringify({
+                refreshtoken:refreshtoken
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            localStorage.setItem('Accesstoken',data.accesstoken);
+            const accesstoken = localStorage.getItem('Accesstoken');
+            fetch('/reportemployee/custom',{
+                method:'post',
+                headers:new Headers({'Content-Type':'application/json',
+                        'authorization':`bearer ${accesstoken}`}),
+                body:JSON.stringify({
+                    field:field.value,
+                    value:value.value
+                })
                 
                 
             })
